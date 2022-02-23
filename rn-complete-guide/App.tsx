bdb3c97d786e1faa679b083621f0fadff02831ad
Akila Mendis <akila@oneit.com.au>
2022-02-23 15:08:02 +0530

    23. Getting Started with Styles

--- rn-complete-guide/App.tsx
+++ rn-complete-guide/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1, padding: 10}}/>
+        <Button title="Add" onPress={()=>console.log("Add Pressed")}/>
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
